de55351a67f54bacc7a06b61794569a71c8393e5
Facundo <wehrlifacundo9@gmail.com>
2023-08-02 09:29:09 -0300

    chore: import Material Module onto Auth Module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthRoutingModule } from './auth-rouitng.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -16,7 +17,8 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
