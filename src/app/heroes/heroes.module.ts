0dcbcdd101117682e1b4640cd0049e1d03f51a7e
Facundo <wehrlifacundo9@gmail.com>
2023-08-03 07:41:47 -0300

    chore: import MaterialModule to use in heroes pages

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,6 +6,7 @@ import { NewPageComponent } from './pages/new-page/new-page.component';
 import { HeroePageComponent } from './pages/heroe-page/heroe-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -19,6 +20,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
   imports: [
     CommonModule,
     HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
