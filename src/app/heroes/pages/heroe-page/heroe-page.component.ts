9e5f345d8b8c74cf5c4b4502eabced4fa48f5fa1
Facundo <wehrlifacundo9@gmail.com>
2023-08-03 12:11:22 -0300

    feat: getting a hero by URL

--- src/app/heroes/pages/heroe-page/heroe-page.component.ts
+++ src/app/heroes/pages/heroe-page/heroe-page.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroe-page',
   templateUrl: './heroe-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HeroePageComponent {
+export class HeroePageComponent implements OnInit {
+  public hero?: Hero;
 
+  constructor(
+    private router: Router,
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes.list']);
+        this.hero = hero;
+        console.log({hero})
+        return;
+      });
+  }
 }
